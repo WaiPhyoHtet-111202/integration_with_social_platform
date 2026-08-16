ddf0933ce8da1f5561f7fa40d24d70ed0b37261d
Your Name <youremail@example.com>
2026-08-16 22:43:27 +0630

    Testing API

--- controllers/telegram_integration.py
+++ controllers/telegram_integration.py
@@ -1,9 +1,47 @@
 from odoo import http
-from odoo.http import request
+from odoo.http import request,Request
+import werkzeug
+import json
 
 class TelegramIntegration(http.Controller):
 
-    @http.route('/api/telegram/webhook',type='http',auth="none",methods=['POST'],csrf=False)
+    def valid_response(self,data,status=200):
+        return werkzeug.wrappers.Response(status=status,content_type='application/json',response=json.dumps(data))
+
+    @http.route('/api/telegram/webhook',type='http',auth="public",methods=['POST'],csrf=False)
     def webhook(self,**kwargs):
-        data = request.jsonrequest
-        print(data)
+        try:
+            data = request.httprequest.data
+            data = json.loads(data)
+            print(f"Data : {data}")
+            customer_account = request.env['customer.account.identity'].sudo()
+            message = data.get('message')
+            message_from = message.get('from')
+            user_id = message_from.get('id')
+            existing_user = customer_account.sudo().search([('external_id','=',user_id)])
+            if existing_user:
+                return
+            user_name = message_from.get('username')
+            name = message_from.get('first_name')
+            platform = 'Telegram'
+            partner = request.env['res.partner'].sudo().create({
+                'name': name,
+                'social_platform': platform,
+            })
+            account = customer_account.create({
+                'user_name' : user_name,
+                'first_name' : name,
+                'external_id' : user_id,
+                'platform' : platform,
+                'partner_id' : partner.id,
+            })
+            return self.valid_response({
+                'success' : True,
+                'message' : 'New Customer Account Created',
+                'id' : account.id,
+            })
+        except Exception as e:
+            return {
+                'status' : 'fail',
+                'message' : str(e)
+            }
--- models/customer_account_identity.py
+++ models/customer_account_identity.py
@@ -4,7 +4,8 @@ class CustomerAccountIdentity(models.Model):
     _name = 'customer.account.identity'
     _description = 'Customer Account Identity'
 
-    name = fields.Char(string='Name')
+    user_name = fields.Char(string='User Name')
+    first_name = fields.Char(string='First Name')
     platform = fields.Char(string='Platform')
-    partner_id = fields.Many2one("res.partner", string='Partner')
+    partner_id = fields.Many2one("res.partner", string='Partner',readonly=True)
     external_id = fields.Char(string='External ID')
